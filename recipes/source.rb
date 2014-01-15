2a5c46d628f5c6ef06ac91fd4cb9c585fd0d1731
Chris Dutton <chris@rosepointnav.com>
2014-01-15 12:10:06 -0800

    removed last change and added ohai reload

--- recipes/source.rb
+++ recipes/source.rb
@@ -43,11 +43,15 @@ else
   end
 end
 
+ohai "reload" do
+  action :reload
+end
+
 gem_package "passenger" do
   version node['passenger']['version']
 end
 
 execute "passenger_module" do
-  command "#{node['passenger']['ruby_bin']} #{node['passenger']['root_path']}/bin/passenger-install-apache2-module _#{node['passenger']['version']}_ --auto"
+  command "#{node['languages']['ruby']['ruby_bin']} #{node['passenger']['root_path']}/bin/passenger-install-apache2-module _#{node['passenger']['version']}_ --auto"
   creates node['passenger']['module_path']
 end
